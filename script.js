56e402fc4700a6833074f0e1659981a160e5a8ae
Md Sajid <sajidcse202024@gmail.com>
2026-06-29 17:29:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// Get the current URL
+const url = location.href;
+
+// Get the length of the URL
+const length = url.length;
+
+// Display the result
+alert("The length of the URL is: " + length);
